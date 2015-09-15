838bcecf0aab792a1b00f41e10001a41d36d72f3
devhq <hqamhiyeh@gmail.com>
2015-09-15 22:59:07 +0000

    Add new action named goodbye

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,6 +4,11 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   
   def hello
-    render text: "hola, mundo!"
+    render text: "¡hola, mundo!"
   end
+  
+  def goodbye
+    render text: "goodbye, world!"
+  end
+  
 end
